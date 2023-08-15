b6547f614c01ee09df50c1a1f8fc5880f631c61d
Hariharan M <hariharan11042001@gmail.com>
2023-08-15 19:09:46 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,24 @@
+"""Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
+
+Example 1:
+
+Input:
+S = i.like.this.program.very.much
+Output: much.very.program.this.like.i
+Explanation: After reversing the whole
+string(not individual words), the input
+string becomes
+much.very.program.this.like.i
+Example 2:
+
+Input:
+S = pqr.mno
+Output: mno.pqr
+Explanation: After reversing the whole
+string , the input string becomes
+mno.pqr"""
+
+def reverseWords(self,S):
+    # code here 
+    final_s1 = ".".join(s.split(".")[::-1])
+    return final_s1
