a5926b4611884216ba1aceb6f9e2c9dce6cf8a3e
Hariharan M <hariharan11042001@gmail.com>
2023-08-19 07:17:51 +0530

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,43 @@
+# Given a list of non negative integers, arrange them in such a manner that they form the largest number possible.The result is going to be very large, hence return the result in the form of a string.
+
+
+# Example 1:
+
+# Input: 
+# N = 5
+# Arr[] = {3, 30, 34, 5, 9}
+# Output: 9534330
+# Explanation: Given numbers are {3, 30, 34,
+# 5, 9}, the arrangement 9534330 gives the
+# largest value.
+# Example 2:
+
+# Input: 
+# N = 4
+# Arr[] = {54, 546, 548, 60}
+# Output: 6054854654
+# Explanation: Given numbers are {54, 546,
+# 548, 60}, the arrangement 6054854654 
+# gives the largest value.
+
+# Your Task:  
+# You don't need to read input or print anything. Your task is to complete the function printLargest() which takes the array of strings arr[] as parameter and returns a string denoting the answer.
+
+# Expected Time Complexity: O(NlogN)
+# Expected Auxiliary Space: O(N)
+
+from functools import cmp_to_key
+
+def printLargest(self,arr):
+    # code here
+    for i,v in enumerate(arr):
+        arr[i] = str(v)
+    
+    def compare(n1,n2):
+        if n1 + n2 > n2 + n1:
+            return -1
+        else:
+            return 1
+    arr = sorted(arr,key=cmp_to_key(compare))
+        
+    return str("".join(arr))
